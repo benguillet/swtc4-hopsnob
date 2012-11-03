ce6386ec904f83ec5bc445094d94e37c19f570d5
Benjamin Guillet <benjamin.guillet@gmail.com>
2012-11-03 01:03:59 -0500

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Swtc4Hopscout::Application.config.secret_token = '2da16c13871d5e080d0ccaf70ee6159ba25985b61c26a085126070c9544efc7ae8afa582b1e80d790176d7e75c4e37dc8b0e349fb756c304f050eb8515831b0e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Swtc4Hopscout::Application.config.session_store :cookie_store, key: '_swtc4-hopscout_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Swtc4Hopscout::Application.config.session_store :active_record_store

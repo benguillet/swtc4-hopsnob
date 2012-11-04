b892967cb8654a2245790d1cd8fac9cb372962a9
Benjamin Guillet <benjamin.guillet@gmail.com>
2012-11-04 14:50:40 -0600

    seeds.rb created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+l = LiquorStore.create(name: 'Zipps Liquor', address: '2618 East Franklin Avenue, Minneapolis, MN')
+l = LiquorStore.create(name: 'Franklin-Nicollet Liquor Store', address: '2012 Nicollet Avenue, Minneapolis, MN')
+
+b = Beer.create(brand: 'Surly Brewing', name: 'Furious')
+b = Beer.create(brand: 'Brau Brothers Brewing', name: 'The Ring Neck')
+
+p = Product.create(beer_id: 1, liquor_store_id: 1)
+p = Product.create(beer_id: 1, liquor_store_id: 2)
+p = Product.create(beer_id: 2, liquor_store_id: 1)
+
